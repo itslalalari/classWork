cdbbccc1f333c30e8b02a7bda24298da6c1b3013
Larissa <giddings.larissa@hotmail.com>
2024-01-16 11:21:10 -0600

    Changed folder path

---
+++ test.js
@@ -0,0 +1,11 @@
+// Larissa Giddings 15Jan2024 JavaScript Test page
+
+console.log("Hello, Andy.");
+
+var example = 100;
+var animal =prompt("Please tell me a type of an animal that you like:");
+
+example = example*prompt("Please enter a number between 1 and 10");
+
+console.log("The 'example' variable is equal to: " + example + 
+"\nas in: if it were possible, I would pet " + example +" " + animal + "s.");
